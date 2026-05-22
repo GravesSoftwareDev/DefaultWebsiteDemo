2a9305c066f7f6c04d9e5508d3d3c90d3f1ef424
Shanna <sg0989463@otc.edu>
2026-05-22 16:32:34 -0500

    changed api url to a proxy model

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { createProxyMiddleware } from 'http-proxy-middleware'
 import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
 
@@ -7,10 +8,20 @@ const __dirname = dirname(__filename)
 
 const app = express()
 const PORT = process.env.PORT || 3000
+const API_URL = process.env.API_URL
+
+if (API_URL) {
+    const proxy = createProxyMiddleware({
+        target: API_URL,
+        changeOrigin: true,
+    })
+    app.use('/endpoints', proxy)
+    app.use('/auth', proxy)
+    app.use('/media', proxy)
+}
 
 app.use(express.static(join(__dirname, 'dist')))
 
-// Send index.html for all routes so React Router works
 app.get('/{*splat}', (_req, res) => {
     res.sendFile(join(__dirname, 'dist', 'index.html'))
 })
