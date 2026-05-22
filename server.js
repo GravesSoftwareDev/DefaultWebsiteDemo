9ed4993a87901291a2e003b4a4842fbb0b162e52
Shanna <sg0989463@otc.edu>
2026-05-22 17:30:47 -0500

    another try

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express'
+import { createProxyMiddleware } from 'http-proxy-middleware'
 import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
-import { createProxyMiddleware } from 'http-proxy-middleware'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
