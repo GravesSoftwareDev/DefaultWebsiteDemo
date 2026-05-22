96bdae8ea7c3517aedc7e80ae506b29ebbb50b56
Shanna <sg0989463@otc.edu>
2026-05-22 15:36:10 -0500

    fix attempt

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-const express = require('express')
-const path = require('path')
+import express from 'express'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(join(__dirname, 'dist')))
 
 // Send index.html for all routes so React Router works
 app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+    res.sendFile(join(__dirname, 'dist', 'index.html'))
 })
 
 app.listen(PORT)
